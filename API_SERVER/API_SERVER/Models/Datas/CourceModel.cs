de3c6b265b9c4a914197be9121bd819fab9025d6
孙昊哲 <sun2000nian@live.com>
2020-12-15 23:48:51 +0800

    update Cource Model

--- API_SERVER/API_SERVER/Models/Datas/CourceModel.cs
+++ API_SERVER/API_SERVER/Models/Datas/CourceModel.cs
@@ -7,6 +7,8 @@ namespace API_SERVER.Models.Datas
 {
     public class CourceModel : BaseOrderModel
     {
-
+        public string Location { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
     }
 }
